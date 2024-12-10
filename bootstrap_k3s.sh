a30af7e074b849bf69e91003ae7d68eff3aaf7cf
alexrf45 <fonalex45@gmail.com>
2024-12-09 21:41:05 -0500

    cluster down

--- bootstrap_k3s.sh
+++ bootstrap_k3s.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# age -d secret-access.enc >secret-access-key
-#
-# age -d access-key-id.enc >access-key
-#
-# age -d pihole.enc >.pihole.env
-#
-# age -d cf.enc >.cf.env
-#
-# age -d secrets.enc >secrets.yaml
-#
+#age -d secret-access.enc >secret-access-key
+
+#age -d access-key-id.enc >access-key
+
+#age -d pihole.enc >.pihole.env
+
+#age -d cf.enc >.cf.env
+
+#age -d secrets.enc >secrets.yaml
+
 #ansible-playbook -i inventory/hosts.yaml -K playbooks/auditing-k3s.yaml
 
 ansible-playbook -i inventory/hosts.yaml -e @secrets.yaml --ask-vault-password -K playbooks/k3s.yaml
 
-# scp sean@10.3.3.6:/etc/rancher/k3s/k3s.yaml $HOME/.kube/config
-#
-# sed -ie s/127.0.0.1/10.3.3.6/g $HOME/.kube/config
-#
+#scp sean@10.3.3.6:/etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+
+#sed -ie s/127.0.0.1/10.3.3.6/g $HOME/.kube/config
+
 # kubectl apply -f ./manifests/namespaces.yaml
 #
-# kubectl create \
-#   -n external-secrets secret generic awssm-secret \
-#   --from-file=./access-key --from-file=./secret-access-key
-#
+# kubectl create secret generic awssm-secret \
+#   --from-file=./access-key --from-file=./secret-access-key \
+#   -n external-secrets
 # kubectl create secret generic pihole-password \
 #   --from-env-file=./.pihole.env \
 #   -n pihole-system
@@ -31,6 +30,18 @@ ansible-playbook -i inventory/hosts.yaml -e @secrets.yaml --ask-vault-password -
 # kubectl create secret generic cloudflare-token-secret \
 #   --from-env-file=./.cf.env \
 #   -n kube-system
+#
+# kubectl create secret generic operator-oauth \
+#   --from-env-file=./.tailscale.env \
+#   -n tailscale-operator
+#
+# kubectl create secret generic harbor-admin-password \
+#   --from-env-file=./.harbor.env \
+#   -n harbor
+#
+# kubectl create secret generic harbor-crypto \
+#   --from-env-file=./.harbor.env \
+#   -n harbor
 #
 # kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
 # kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
@@ -43,8 +54,7 @@ ansible-playbook -i inventory/hosts.yaml -e @secrets.yaml --ask-vault-password -
 # kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
 # kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
 #
-# k create secret generic basic-auth --from-file=auth -n monitoring
-# k create secret generic basic-auth --from-file=auth -n longhorn-system
-# k create secret generic harbor-admin-password --from-env-file=./.harbor.env -n harbor
-#
-# k create secret generic harbor-crypto --from-env-file=./.harbor.env -n harbor
+# #k create secret generic basic-auth --from-file=auth -n monitoring
+# #kubectl create secret generic basic-auth --from-file=auth -n storage
+# #kubectl create secret generic harbor-admin-password --from-env-file=./.harbor.env -n harbor
+# #kubectl create secret generic harbor-crypto --from-env-file=./.harbor.env -n harbor
