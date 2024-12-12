4f38c325848838a4b0763ab89317d6fea110ec00
alexrf45 <fonalex45@gmail.com>
2024-12-12 08:56:54 -0500

    reset. moving to RHEL

--- bootstrap_k3s.sh
+++ bootstrap_k3s.sh
@@ -1,60 +1,64 @@
 #!/bin/bash
 
-#age -d secret-access.enc >secret-access-key
-
-#age -d access-key-id.enc >access-key
-
-#age -d pihole.enc >.pihole.env
-
-#age -d cf.enc >.cf.env
-
-#age -d secrets.enc >secrets.yaml
-
+# age -d secret-access.enc >secret-access-key
+#
+# age -d access-key-id.enc >access-key
+#
+# age -d pihole.enc >.pihole.env
+#
+# age -d cf.enc >.cf.env
+#
+# age -d harbor.enc >.harbor.env
+#
+# age -d secrets.enc >secrets.yaml
+#
 #ansible-playbook -i inventory/hosts.yaml -K playbooks/auditing-k3s.yaml
 
 ansible-playbook -i inventory/hosts.yaml -e @secrets.yaml --ask-vault-password -K playbooks/k3s.yaml
 
-#scp sean@10.3.3.6:/etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+scp osiris:/etc/rancher/k3s/k3s.yaml $HOME/.kube/config
 
-#sed -ie s/127.0.0.1/10.3.3.6/g $HOME/.kube/config
+sed -ie s/127.0.0.1/10.3.3.10/g $HOME/.kube/config
 
-# kubectl apply -f ./manifests/namespaces.yaml
+kubectl apply -f ./manifests/namespaces.yaml
 #
-# kubectl create secret generic awssm-secret \
-#   --from-file=./access-key --from-file=./secret-access-key \
-#   -n external-secrets
-# kubectl create secret generic pihole-password \
-#   --from-env-file=./.pihole.env \
-#   -n pihole-system
+kubectl create secret generic awssm-secret \
+  --from-file=./access-key --from-file=./secret-access-key \
+  -n external-secrets
+kubectl create secret generic pihole-password \
+  --from-env-file=./.pihole.env \
+  -n pihole-system
 #
-# kubectl create secret generic cloudflare-token-secret \
-#   --from-env-file=./.cf.env \
-#   -n kube-system
+kubectl create secret generic cloudflare-token-secret \
+  --from-env-file=./.cf.env \
+  -n kube-system
 #
-# kubectl create secret generic operator-oauth \
-#   --from-env-file=./.tailscale.env \
-#   -n tailscale-operator
+kubectl create secret generic operator-oauth \
+  --from-env-file=./.tailscale.env \
+  -n tailscale-operator
 #
-# kubectl create secret generic harbor-admin-password \
-#   --from-env-file=./.harbor.env \
-#   -n harbor
+kubectl create secret generic harbor-admin-password \
+  --from-env-file=./.harbor.env \
+  -n harbor
 #
-# kubectl create secret generic harbor-crypto \
-#   --from-env-file=./.harbor.env \
-#   -n harbor
+kubectl create secret generic harbor-crypto \
+  --from-env-file=./.harbor.env \
+  -n harbor
 #
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
-# kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.78.1/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+
+kubectl label node horus bastet anubis node-role.kubernetes.io/worker=true
 #
-# #k create secret generic basic-auth --from-file=auth -n monitoring
-# #kubectl create secret generic basic-auth --from-file=auth -n storage
-# #kubectl create secret generic harbor-admin-password --from-env-file=./.harbor.env -n harbor
-# #kubectl create secret generic harbor-crypto --from-env-file=./.harbor.env -n harbor
+#k create secret generic basic-auth --from-file=auth -n monitoring
+#kubectl create secret generic basic-auth --from-file=auth -n storage
+#kubectl create secret generic harbor-admin-password --from-env-file=./.harbor.env -n harbor
+#kubectl create secret generic harbor-crypto --from-env-file=./.harbor.env -n harbor
--- helm/install.sh
+++ helm/install.sh
@@ -4,12 +4,12 @@ helmfile init
 
 helmfile apply
 
-kubectl apply -f ../manifests/nginx.yaml
+# kubectl apply -f ../manifests/nginx.yaml
+#
+# kubectl apply -f ../manifests/metallb.yaml
+#
+# kubectl apply -f ../manifests/cert-manager.yaml
 
-kubectl apply -f ../manifests/metallb.yaml
-
-kubectl apply -f ../manifests/cert-manager.yaml
-
-kubectl apply -f ../manifests/external-secrets.yaml
+#kubectl apply -f ../manifests/external-secrets.yaml
 
 #kubectl apply ../manifests/
